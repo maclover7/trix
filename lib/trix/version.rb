a130180a2308abf65b49295f1c02ecac14a397e4
Mateo Murphy <mateo.murphy@gmail.com>
2016-02-07 17:25:39 -0500

    Update for compatibility with earlier version of Rails
    - Improve specs
    - Add appraisals to spec against multiple versions
    - Update travis config

--- lib/trix/version.rb
+++ lib/trix/version.rb
@@ -1,3 +1,3 @@
 module Trix
-  VERSION = '0.9.5'
+  VERSION = '0.9.5'.freeze
 end
---
+++ spec/trix_editor_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'trix/form'
+
+describe TrixEditorHelper, type: :helper do
+  describe '#trix_editor' do
+    it 'returns a trix editor tag' do
+      blog = mock_model('Blog', text: 'Description')
+
+      assign(:blog, blog)
+      expect(helper.trix_editor(:blog, :text)).to include('trix-editor')
+    end
+  end
+
+  describe '#trix_editor_tag' do
+    it 'returns a trix editor tag' do
+      expect(helper.trix_editor_tag('text')).to include('trix-editor')
+    end
+  end
+end
